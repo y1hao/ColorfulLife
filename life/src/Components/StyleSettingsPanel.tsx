3f55e92f0baaa950f267d710a66b33e59fb7f0ad
CoderYihaoWang <yesiamyihao@gmail.com>
2020-07-30 12:14:41 +1200

    Extract higher order component on the styles settings panel

--- life/src/Components/StyleSettingsPanel.tsx
+++ life/src/Components/StyleSettingsPanel.tsx
@@ -16,72 +16,83 @@ const useStyles = makeStyles({
     }
 })
 
-export default function StyleSettingsPanel(props: IProps) {
-    const classes = useStyles();
+function makeSettingsPanel(tabName: "alive" | "dead") {
+    const index = tabName === "dead" ? 0 : 1;
+    return function(props: IProps) {
+        const [size, setSize] = useState<number>()
+        const [shape, setShape] = useState<number>()
+        const [color, setColor] = useState<string>()
+        const [background, setBackground] = useState<string>()
+        const [borderWidth, setBorderWidth] = useState<number>()
+        const [borderColor, setBorderColor] = useState<string>()
+        const [elevation, setElevation] = useState<number>()
+        
+        const handleSetSize = (index: number, size: number) => {
     
-    const [tab, setTab] = useState<number>(0);
-    const [isAdvancedSettingsOpen, setIsAdvancedSettingsOpen] = useState<boolean>(false);
-
-    const handleSetSize = (index: number, size: number) => {
-
-    }
-
-    const handleSetShape = (index: number, size: number) => {
-
-    }
-
-    const handleSetColor = (index: number, size: number) => {
-
-    }
-
-    const handleSetBackground = (index: number, size: number) => {
-
-    }
-
-    const handleSetBorderWidth = (index: number, size: number) => {
-
-    }
-
-    const handleSetBorderColor = (index: number, size: number) => {
-
-    }
-
-    const handleSetElevation = (index: number, size: number) => {
-
-    }
-
-    const Settings = (tabName: "alive" | "dead") => {
-        const index = tabName === "dead" ? 0 : 1
+        }
+    
+        const handleSetShape = (index: number, shape: number) => {
+    
+        }
+    
+        const handleSetColor = (index: number, size: number) => {
+    
+        }
+    
+        const handleSetBackground = (index: number, size: number) => {
+    
+        }
+    
+        const handleSetBorderWidth = (index: number, size: number) => {
+    
+        }
+    
+        const handleSetBorderColor = (index: number, size: number) => {
+    
+        }
+    
+        const handleSetElevation = (index: number, size: number) => {
+    
+        }
+    
         return <div>
             <InputTitle>
                 Size
             </InputTitle>
-
+    
             <InputTitle>
                 Shape
             </InputTitle>
-
+    
             <InputTitle>
                 Color
             </InputTitle>
-
+    
             <InputTitle>
                 Background
             </InputTitle>
-
+    
             <InputTitle>
                 Border Color
             </InputTitle>
-
+    
             <InputTitle>
                 Border Width
             </InputTitle>
-
+    
             <InputTitle>
                 Elevation
             </InputTitle>
         </div> 
     }
+}
+
+
+export default function StyleSettingsPanel(props: IProps) {
+    const classes = useStyles();
+    
+    const [tab, setTab] = useState<number>(0);
+    const [isAdvancedSettingsOpen, setIsAdvancedSettingsOpen] = useState<boolean>(false);
 
     return <div>
         <Tabs
@@ -92,10 +103,10 @@ export default function StyleSettingsPanel(props: IProps) {
             <Tab label="Dead" className={classes.tab}/>
         </Tabs>    
         <div hidden={tab !== 0}>
-            { Settings("alive") }
+            { makeSettingsPanel("alive")(props) }
         </div>
         <div hidden={tab !== 1}>
-            { Settings("dead") }
+            { makeSettingsPanel("dead")(props) }
         </div>
         <Button onClick={() => setIsAdvancedSettingsOpen(true)}>
             Advanced Settings
